c2bd8e5a7d888c33c120b405c2495a5d840fd151
tjlaboss <travis@ktc-technology.com>
2016-09-15 17:12:53 -0400

    Moved forward with development of openmc.pwr module

---
+++ assembly.py
@@ -0,0 +1,435 @@
+# PWR Assembly
+# 
+# Module for the OpenMC Python API. Once developed, it should
+# contain everything needed to generate an openmc.Universe containing
+# a model of a Westinghouse-style PWR assembly
+
+import openmc
+from functions import fill_lattice
+from copy import copy
+from math import sqrt
+
+# Global constants for counters
+SURFACE, CELL, MATERIAL, UNIVERSE = range(-1,-5,-1)
+# Global variables for counters
+openmc_surface_count	= openmc.AUTO_SURFACE_ID + 1
+openmc_cell_count 		= openmc.AUTO_CELL_ID + 1
+openmc_material_count	= openmc.AUTO_MATERIAL_ID + 1
+openmc_universe_count	= openmc.AUTO_UNIVERSE_ID + 1
+
+
+class Mixture(openmc.Material):
+	'''Mixture of multiple OpenMC materials.
+	Designed to be functionally identical to a regular openmc.Material,
+	but defined differently.
+	
+	WARNING: Currently only works for weight fractions and densities in
+	grams per cubic centimeter (g/cc and g/cm^3).
+	
+	
+	Parameters
+    ----------
+	
+	materials:		list of instances of openmc.Material to mix
+    vfracs:			list of floats describing the volume fraction of each
+    				Material listed above.  
+    
+    material_id:	int, optional
+        Unique identifier for the material. If not specified, an identifier will
+        automatically be assigned.
+    name:			str, optional
+        Name of the material. If not specified, the name will be the empty
+        string.
+       
+    
+    Attributes
+    ----------
+    id : int
+        Unique identifier for the material
+    density : float
+        Density of the material (units defined separately)
+    density_units : str
+        Units used for `density`. Can be one of 'g/cm3', 'g/cc', 'kg/cm3',
+        'atom/b-cm', 'atom/cm3', 'sum', or 'macro'.  The 'macro' unit only
+        applies in the case of a multi-group calculation.
+    elements : list of tuple
+        List in which each item is a 3-tuple consisting of an
+        :class:`openmc.Element` instance, the percent density, and the percent
+        type ('ao' or 'wo').
+    nuclides : list of tuple
+        List in which each item is a 3-tuple consisting of an
+        :class:`openmc.Nuclide` instance, the percent density, and the percent
+        type ('ao' or 'wo').
+	
+	'''
+	
+	def __init__(self, materials, vfracs, material_id = None, frac_type = 'wo', name = ""):
+		super(Mixture, self).__init__(material_id, name)
+		
+		mix_isos = []
+		density = 0.0
+	
+		for i in range(len(materials)):
+			density += materials[i].density * (vfracs[i] / sum(vfracs))
+		for i in range(len(materials)):
+			mat = materials[i]
+			#mat.convert_ao_to_wo() --> Exists in VERA-to-OpenMC, but not here
+			wtf = vfracs[i]*mat.density 	# weight fraction of entire material
+			for iso in mat.get_all_nuclides().values():
+				nuclide = iso[0]
+				new_wt = wtf*iso[1] / density
+				if iso in mix_isos:
+					old_wt = mix_isos[iso][1]
+					mix_isos.append((nuclide, new_wt + old_wt, frac_type))
+				else:
+					mix_isos.append((nuclide, new_wt, frac_type))
+					
+		self._nuclides = mix_isos
+		self.set_density("g/cc", density)
+
+
+
+class Nozzle(object):
+	'''Nozzle defined as a smeared material of a certain height and mass
+	
+	Parameters:
+		height:		float; z-coordinate of the top of this nozzle.
+					The coordinate of the bottom is determined automatically.
+		mass:		float; mass in grams of nozzle material
+		nozzle_mat:	instance of openmc.Material; composition of the nozzle itself
+		mod_mat:	instance of openmc.Material; composition of the moderator
+		npins:		integer; number of pins in a row. Used to calculate Nozzle area
+		pitch:		float; pitch in cm between pins.  Used to calculate Nozzle area
+		[name:		string; optional name for the nozzle. Default is "nozzle-material".]
+	
+	Attributes:
+		height:		[same as above]
+		mass:		[same as above]
+		material:	instance of openmc.Material; smearing of nozzle_mat and mod_mat
+	'''
+	
+	def __init__(self, height, mass, nozzle_mat, mod_mat, npins, pitch, name = "nozzle-material"):
+		self.height = height
+		self.mass = mass
+		self.name = name
+		volume = (npins*pitch)**2 * height
+		self.material = self.__mix(nozzle_mat, mod_mat, volume)
+		
+	def __mix(self, mat, mod, v):
+		'''Mix materials in the way necessary to create the nozzle.
+		
+		WARNING: Currently only supports the same type of fraction (weight or atomic)
+		
+		Inputs:
+			mat:	instance of openmc.Material describing the nozzle composition
+			mod:	instance of openmc.Material describing the moderator
+			v:		float; total volume in cm^3 of the nozzle region
+		
+		Output:
+			mix:	instance of openmc.Material describing the smearing of 'mat' and 'mod'
+		'''
+		mat_vol = self.mass / mat.density
+		mod_vol = v - mat_vol
+		vfracs = [mat_vol / v, mat_vol / v]
+		
+		material = Mixture((mat, mod), vfracs, name = self.name)
+		return material
+		
+	def __str__(self):
+		return self.name
+
+
+
+
+
+def counter(count):
+		'''Get the next cell/surface/material/universe number, and update the counter.
+		Input:
+			count:		CELL, SURFACE, MATERIAL, or UNIVERSE
+		Output:
+			integer representing the next cell/surface/material/universe ID'''
+		if count == SURFACE:
+			global openmc_surface_count
+			openmc_surface_count += 1
+			return openmc_surface_count
+		elif count == CELL:
+			global openmc_cell_count
+			openmc_cell_count += 1
+			return openmc_cell_count
+		elif count == MATERIAL:
+			global openmc_cell_count
+			openmc_material_count += 1
+			return openmc_material_count
+		elif count == UNIVERSE:
+			global openmc_cell_count
+			openmc_universe_count += 1
+			return openmc_universe_count
+		else:
+			raise IndexError("Index " + str(count) + " is not SURFACE, CELL, MATERIAL, or UNIVERSE.")
+
+"""
+def duplicate(orig):
+	'''Copy an OpenMC object, except for a new id 
+	
+	Input:
+		orig: instance of openmc.(Surface, Cell, Material, or Universe)
+	
+	Output:
+		dupl: same, but with a different instance.id 
+	'''
+	dup = copy(orig)
+	if isinstance(orig, openmc.Surface):
+		dup.id = openmc.AUTO_SURFACE_ID
+		openmc.AUTO_SURFACE_ID += 1
+	elif isinstance(orig, openmc.Cell):
+		dup.id = openmc.AUTO_CELL_ID
+		openmc.AUTO_CELL_ID += 1
+	elif isinstance(orig, openmc.Material):
+		dup.id = openmc.AUTO_MATERIAL_ID
+		openmc.AUTO_MATERIAL_ID += 1
+	elif isinstance(orig, openmc.Universe):
+		dup.id = openmc.AUTO_UNIVERSE_ID
+		openmc.AUTO_UNIVERSE_ID += 1
+	else:
+		name = orig.__class__.__name__
+		raise TypeError(str(orig) + " is an instance of " + name + 
+					"; expected Surface, Cell, Material, or Universe")
+	return dup
+"""
+def duplicate(orig):
+	'''Copy an OpenMC object, except for a new id 
+	
+	Input:
+		orig: instance of openmc.(Surface, Cell, Material, or Universe)
+	
+	Output:
+		dupl: same, but with a different instance.id 
+	'''
+	dup = copy(orig)
+	if isinstance(orig, openmc.Surface):
+		dup.id = counter(SURFACE)
+	elif isinstance(orig, openmc.Cell):
+		dup.id = counter(CELL)
+	elif isinstance(orig, openmc.Material):
+		dup.id = counter(MATERIAL)
+	elif isinstance(orig, openmc.Universe):
+		dup.id = counter(UNIVERSE)
+	else:
+		name = orig.__class__.__name__
+		raise TypeError(str(orig) + " is an instance of " + name + 
+					"; expected Surface, Cell, Material, or Universe")
+	return dup
+
+
+class SpacerGrid(object):
+	'''Object to hold properties of an assembly's spacer grids
+	
+	Parameters:
+		key: 		string; unique name of this spacer grid
+		height:		float; height (cm) of the spacer around the pins
+		mass:		float; mass in g of the entire spacer grid's material
+		material:	instance of class openmc.Material
+		pitch:		float; pin pitch (cm) 
+		npins:		number of pins across an assembly
+	Attributes:
+		(key, height, mass, material - as above)
+		thickness:	float; thickness (cm) of the grid around each pin, 
+					or half the total thickness between pins
+		'''
+	
+	def __init__(self, key, height, mass, material, pitch, npins):
+		self.key = key	
+		self.height = height	
+		self.mass = mass		
+		self.material = material
+		self.thickness = self.calculate_thickness(pitch, npins)
+	
+	def calculate_thickness(self, pitch, npins):
+		'''Calculate the thickness of the spacer surrounding each pincell.
+		Inputs:
+			pitch:		float; pin pitch (cm)
+			npins:		int; number of pins across Assembly (npinsxnpins)
+		'''
+		
+		''' Method:
+		
+		Volume = mass / density;		Combined Area = Volume / height
+			Therefore, [ A = m/rho/h ],		and the area around a single pincell:
+												a = m/rho/h / npins^2
+			
+			The area of the spacer material around one cell can also be found:
+				a = p^2 - (p - 2*t)^2,		where 't' is the thickness and 'p' is the pitch
+			->  a = p^2 - [p^2 - 4*t*p + 4*t^2]
+			->  a = 4*t*p - 4*t^2
+				
+			Equate the two expressions for a:
+				m/rho/h / npins^2 = 4*t*p - 4*t^2
+			Then solve for 't' using the quadratic equation:
+			
+			              [             (          m/rho/h   ) ]
+				t = 0.5 * [ p  +/- sqrt ( p^2 -   ---------- ) ]
+				          [             (          npins^2   ) ]
+        '''
+		
+		A = self.mass / self.materials[self.material].density / self.height
+		t = 0.5*(pitch - sqrt(pitch**2 - A/npins**2))
+		return t
+		
+	def __str__(self):
+		name = self.key + ': ' + str(self.thickness) + " cm"
+		return name
+
+
+def add_grid_to(pincell, pitch, t, material):
+	'''Given a pincell to be placed in a lattice, add
+	the spacer grid to the individual cell.
+	
+	Inputs:
+		pincell:	instance of openmc.Universe describing the pincell
+					and its concentric rings of instances of openmc.Cell
+		pitch:		float; pin pitch in cm
+		t:			float; thickness in cm of one edge of the spacer between
+					two pincells (HALF the total thickness)
+		material:	instance of openmc.Material from which the spacer is made
+	
+	Output:
+		new_cell:	instance of openmc.Universe describing the pincell
+					surrounded by the spacer
+	'''
+	assert isinstance(pincell, openmc.Universe), str(pincell) + "must be an openmc.Universe (not a Cell)"
+	assert isinstance(material, openmc.Material), str(material) + "is not an instance of openmc.Material" 
+	
+	orig_list = list(pincell.cells.values())
+	
+	# Create necessary planes
+	p = pitch / 2.0
+	top_out = openmc.YPlane(y0 =  p)
+	top_in  = openmc.YPlane(y0 =  p - t)
+	bot_in  = openmc.YPlane(y0 = -p + t)
+	bot_out = openmc.YPlane(y0 = -p)
+	left_out  = openmc.XPlane(x0 = -p)		# He feels left out
+	left_in   = openmc.XPlane(x0 = -p + t)
+	right_in  = openmc.XPlane(x0 =  p - t)
+	right_out = openmc.XPlane(x0 =  p)
+	
+	# Get the outermost (mod) Cell of the pincell
+	mod_cell = duplicate(orig_list[-1])
+	
+	# Make a cell encompassing the 4 sides of the spacer
+	spacer = openmc.Cell(name = pincell.name + " spacer")
+	spacer.region = (+left_out	& +top_in 	& -top_out	&	-right_out) | \
+					(+right_in	& -right_out& +bot_in	& 	-top_in)	| \
+					(+left_out	& -left_in	& +bot_in	&	-top_in)	| \
+					(+bot_out 	& -bot_in	& +left_out	&	-right_out )  
+					#& mod_cell.region	# top; bottom; right; left; #outside cylinder	 
+	spacer.fill = material
+	
+	# Then fix the moderator cell to be within the bounds of the spacer
+	mod_cell.region = mod_cell.region & \
+					(+bot_in	& +left_in	& -top_in	& -right_in )
+	
+	new_cell = openmc.Universe(name = pincell.name + " gridded")
+	# Add all of the original cells except the old mod cell
+	for i in range(len(orig_list) - 1):
+		new_cell.add_cell(orig_list[i])
+	new_cell.add_cell(mod_cell) 	# the new mod cell
+	new_cell.add_cell(spacer)
+	
+	return new_cell
+
+
+class Assembly(object):
+	'''An OpenMC Universe containing cells for the upper/lower nozzles,
+	lattices (with and without spacer grids), and surrounding moderator.
+	
+	Parameters (all optional except "key"):
+		key:			str; short, unique name of this Assembly as will appear in the core lattice.
+		name:			str; more descriptive name of this Assembly, if desired
+						[Default: same as key]
+		universe_id:	int; unique integer identifier for its OpenMC universe
+						[Default: None, and will be assigned automatically at instantiation of openmc.Universe]
+		lattices:		list of instances of openmc.RectLattice, in the axial order they appear in the assembly
+						(bottom -> top).
+						[Default: empty list]
+		lattice_elevs:	list of floats describing the elevations (cm) of each boundary in 'lattices',
+						relative to the bottom core plate. The next lattice starts where the last leaves off.
+						**Must contain exactly len(lattices)+1 entries**
+						[Default: empty list] 
+		spacers:		list of instances of SpacerGrid, in the axial order they appear in the assembly
+						(bottom -> top). 
+						[Default: empty list]
+		spacer_elevs:	list of floats describing the elevations (cm) of the midpoint of each spacer grid
+						in 'spacers', relative to the bottom core plate. Gaps are expected.
+						***Must contain exactly len(spacers) entries**
+						[Default: empty list]
+		lower_nozzle:	instance of Nozzle, starting at z=0 and terminating at min(lattice_elevs)
+						[Default: None]
+		upper_nozzle:	instance of Nozzle, starting at max(lattice_elevs) and terminating at z += Nozzle.height
+						[Default: None]
+		mod:			instance of openmc.Material describing  moderator surrounding the assembly.
+						[Default: None] 
+	
+	Attributes:
+		All the above
+	'''
+
+	def __init__(self, key = "", name = "", universe_id = None,
+				lattices = [], lattice_elevs = [], spacers = [], spacer_elevs = [],
+				lower_nozzle = None, upper_nozzle = None, 
+				mod = None):
+		
+		return None
+	
+	
+	def __prebuild(self):
+		'''Check that all the required properties are there.'''
+	
+	
+	def build(self):
+		'''Construct the assembly from the ground up.
+		
+		Output:
+			instance of openmc.Universe'''
+		
+		self.__prebuild()
+		
+		return None
+		
+
+
+
+
+# Test
+if __name__ == '__main__':
+	
+	
+	
+	# Define a global test moderator
+	mod = openmc.Material(1, "mod")
+	mod.set_density("g/cc", 1.0)
+	mod.add_nuclide("h-1", 2.0/3, 'ao')
+	mod.add_nuclide("o-16", 1.0/3, 'ao')
+	
+	# Define a simple test material
+	iron = openmc.Material(2, "iron")
+	iron.set_density("g/cc", 7.8)
+	iron.add_element("Fe", 1, 'ao', expand=True)
+	
+	mix1 = Mixture([mod, iron], [0.5,0.5], 33, 'watery iron')
+	assert isinstance(mix1, openmc.Material)	
+	noz1 = Nozzle(10, 6250, iron, mod, 1, 10)
+
+	# Test a pincell
+	cyl0 = openmc.ZCylinder(10, R = 0.300) 
+	cyl1 = openmc.ZCylinder(11, R = 0.333)
+	cyl2 = openmc.ZCylinder(12, R = 0.350)
+	ring0 = openmc.Cell(100, fill = iron, region = -cyl0)
+	ring1 = openmc.Cell(101, fill = mod, region = (-cyl1 & +cyl0) )
+	ring2 = openmc.Cell(102, fill = mix1, region = (-cyl2 & +cyl1) )
+	outer = openmc.Cell(199, fill = mod, region = +cyl2)
+	uni = openmc.Universe(cells = (ring0, ring1, ring2, outer), name = "test pincell")
+	print(uni)
+	gridded = add_grid_to(uni, 1.0, 0.10, iron)
+	print(gridded)
+	#print(duplicate(uni))
+
