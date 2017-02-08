03adad39e1516308158a1afa91c3da87372fbb9a
tjlaboss <travis@ktc-technology.com>
2017-02-08 14:52:52 -0500

    Added neutron pad file

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from pwr.assembly import *
 from pwr.baffle import *
 from pwr.spacergrid import *
 from pwr.nozzle import *
+from pwr.neutron_pad import Pad
 from pwr.functions import *
 from pwr.mixture import *
-from pwr.counter import *
+from pwr.counter import Counter
---
+++ neutron_pad.py
@@ -0,0 +1,9 @@
+# Neutron Pad
+#
+# Class for a PWR neutron pad
+
+class Pad(object):
+	"""Neutron pad as found in the reactor vessel of a PWR.
+	
+	"""
+	
